e9ce7d1c269621d17c9d9a0cf2a5b14becb4700b
t-ishitsuka <takuya.ishitsuka@gmail.com>
2025-03-22 20:46:57 +0900

    feat: vo

--- backend/api/src/presentation/handlers/api/v1/healthcheck/HealthCheckHandler.ts
+++ backend/api/src/presentation/handlers/api/v1/healthcheck/HealthCheckHandler.ts
@@ -1,12 +1,15 @@
 import { Hono } from "hono";
+import { StringValueObject } from "~/domains/libs/base/StringValueObject";
+import { HealthCheckResponse } from "~/presentation/handlers/api/v1/healthcheck/dto/HealthCheckResponse";
 
 const healthCheckHandler = new Hono()
   .basePath("/api/v1/health-check")
   .get("/", (c) => {
-    return c.json({ message: "health check!" });
-  })
-  .get("/2", (c) => {
-    return c.json({ message: "health check 2!" });
+    return c.json(
+      new HealthCheckResponse({
+        message: new StringValueObject("ok"),
+      })
+    );
   });
 
 export { healthCheckHandler };
